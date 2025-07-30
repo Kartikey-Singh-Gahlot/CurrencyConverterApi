475054e23845689d57e075fb970d2362fb100048
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-07-30 10:16:37 +0530

    quantity feature added

--- index.js
+++ index.js
@@ -21,18 +21,18 @@ setConnection().then(()=>{
 
 
 app.get("/currency/rates", async (req, res)=>{
-   let {c1,c2} = req.query;
+   let {c1,c2,q} = req.query;
    let one  = await currencyModel.find({currencyCode:c1.toUpperCase()});
    let two = await currencyModel.find({currencyCode:c2.toUpperCase()});
    if(c2 == 'USD'){
-     res.send({ rate : one[0].rate});
+     res.send({ rate : q*(one[0].rate)});
    }
    else if (c1 == c2){
-     res.send({rate :1});
+     res.send({rate :q});
    }
    else{
      let r = two[0].rate/ one[0].rate;
-     res.send({rate:r});
+     res.send({rate:q*r});
    }
 });
 
@@ -40,6 +40,7 @@ app.get("/currency/all", async (req, res)=>{
   let data = await currencyModel.find({});
   res.send(data);
 });
+
 
 
 module.exports = app;
