d7a8788599a784a50081913934436f2c7dea35bf
kartikeyGehlot2328 <kartikey.22bcon777@jecrcu.edu.in>
2025-08-17 10:31:58 +0530

    initaited full stack 01

--- Routes/Routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const express = require("express");
-const app = express();
-const {fetchRate, fetchAll} = require("../backend/Controllers/UtilityControllers.js")
-
-router.get("/rates", fetchRate);
-router.get("/all", fetchAll );
-
-module.exports = router;
